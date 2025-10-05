a0e7cb626a6ee4f5f14e20645b6a2a886340a5b9
RanjeetHBhosale <ranjeetbhosale165@gmail.com>
2025-10-05 14:13:47 +0530

    Added Calculator for Add

---
+++ CalculatorTest.java
@@ -0,0 +1,15 @@
+ class Calculator{
+
+public int add(int a, int b){
+      return a + b;
+}
+
+}
+public class CalculatorTest{
+
+public static void main(String args[]){
+Calculator c=new Calculator();
+System.out.println("Result Of addition is: "+ c.add(1,6));
+}
+
+}

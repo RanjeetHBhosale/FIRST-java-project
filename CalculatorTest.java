8026155b05ae6530075942dbd7811638c09c234b
RanjeetHBhosale <ranjeetbhosale165@gmail.com>
2025-10-05 23:30:23 +0530

    added substraction in calculator

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -3,13 +3,18 @@
 public int add(int a, int b){
       return a + b;
 }
+public int sub(int a, int b){
+      return a - b;
+}
 
 }
 public class CalculatorTest{
 
 public static void main(String args[]){
 Calculator c=new Calculator();
+
 System.out.println("Result Of addition is: "+ c.add(1,6));
+System.out.println("Result Of addition is: "+ c.sub(7,6));
 }
 
 }

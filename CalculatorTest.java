452d22c42b9172df22899ded92046c0466157f16
RanjeetHBhosale <ranjeetbhosale165@gmail.com>
2025-10-06 00:02:17 +0530

    added multiplication

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -6,7 +6,9 @@ public int add(int a, int b){
 public int sub(int a, int b){
       return a - b;
 }
-
+public int mul(int a, int b){
+      return a * b;
+}
 }
 public class CalculatorTest{
 
@@ -14,7 +16,8 @@ public static void main(String args[]){
 Calculator c=new Calculator();
 
 System.out.println("Result Of addition is: "+ c.add(1,6));
-System.out.println("Result Of addition is: "+ c.sub(7,6));
+System.out.println("Result Of subtraction is: "+ c.sub(7,6));
+System.out.println("Result Of multiplication is: "+ c.mul(7,6));
 }
 
 }
